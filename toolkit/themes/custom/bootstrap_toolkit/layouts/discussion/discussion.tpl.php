4ae6edf8eacd922f10dc63156be58c1a4d824338
IshaDakota <adam@marchinglizard.net>
2015-01-03 17:00:38 -0600

    Add Discussion Guide page - templates, styles, and views.

---
+++ toolkit/themes/custom/bootstrap_toolkit/layouts/discussion/discussion.tpl.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @file
+ * Template for Discussion Page.
+ *
+ * Variables:
+ * - $css_id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ * panel of the layout. This layout supports the following sections:
+ */
+?>
+
+<div class="panel-display discussion clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="row">
+    <div class="col-md-8 content panel-panel">
+      <div class="panel-panel-inner">
+        <?php print $content['contentmain']; ?>
+      </div>
+    </div>
+    <div class="col-md-4 sidebar panel-panel">
+      <div class="row">
+        <div class="panel-panel-inner jumbotron">
+          <div>This discussion guide is referenced in:</div>
+          <?php print $content['sidebartop']; ?>
+        </div>
+      </div>
+      <div class="row">
+        <div class="panel-panel-inner">
+          <?php print $content['sidebarbottom']; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div><!-- /.resource -->
