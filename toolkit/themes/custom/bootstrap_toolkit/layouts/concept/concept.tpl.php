ee032604625e82b8ee2897dd9f1afcf0be25c384
IshaDakota <adam@marchinglizard.net>
2015-06-19 10:09:04 -0500

    Draft of Concept content type with some adjustments to resource as well.

--- toolkit/themes/custom/bootstrap_toolkit/layouts/concept/concept.tpl.php
+++ toolkit/themes/custom/bootstrap_toolkit/layouts/concept/concept.tpl.php
@@ -10,25 +10,43 @@
  */
 ?>
 
-<div class="panel-display concept clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="row">
-    <div class="col-md-8 content panel-panel">
-      <div class="panel-panel-inner">
-        <?php print $content['contentmain']; ?>
-      </div>
-    </div>
-    <div class="col-md-4 sidebar panel-panel">
+  <section class="content-abstract">
+    <div class="container">
       <div class="row">
-        <div class="panel-panel-inner jumbotron">
-          <div>This educator tool is referenced in:</div>
-          <?php print $content['sidebartop']; ?>
+        <div class="col-md-12 panel-panel highlighted">
+          <div class="panel-panel-inner">
+            <?php print $content['title']; ?>
+            <div class="hr"></div>
+            <div class="concept-subtitle">Toolkit Concept</div>
+          </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="panel-panel-inner">
-          <?php print $content['sidebarbottom']; ?>
-        </div>
-      </div>
+      </div>     
     </div>
-  </div>
-</div><!-- /.resource -->
+  </section>
+  
+  <section class="inner-page">
+    <div class="container page-content">
+    <a id="main-content"></a>        
+      <div class="row">
+        <div class="col-md-8 content panel-panel">
+          <div class="panel-panel-inner">
+            <?php print $content['contentmain']; ?>
+          </div>
+        </div>
+        <div class="col-md-4 sidebar panel-panel">
+          <div class="panel-panel-inner">
+           <div class="row">
+            <div class="panel-panel-inner jumbotron">
+              <div class="resource-description">This piece is part of a larger set of <a href="/questions-concepts">Questions and Concepts</a> in Conflict Education - a collection of essays based in the framework of the <a href="http://www.theicenter.org/aleph-bet">Aleph-Bet of Israel Education</a>.</div>
+              <div class="resource-reference">This concept piece is referenced in:</div>
+              <?php print $content['sidebartop']; ?>
+            </div>
+          </div>
+        <div class="row">
+          <div class="panel-panel-inner">
+            <?php print $content['sidebarbottom']; ?>
+          </div>
+        </div>
+      </div>    
+    </div>
+  </section>
