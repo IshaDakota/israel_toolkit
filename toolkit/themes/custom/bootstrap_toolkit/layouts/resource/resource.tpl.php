252f0710a22453b175a3f301aca50a4af0e1350b
IshaDakota <adam@marchinglizard.net>
2015-06-19 07:30:27 -0500

    WIP: Resource content type.

--- toolkit/themes/custom/bootstrap_toolkit/layouts/resource/resource.tpl.php
+++ toolkit/themes/custom/bootstrap_toolkit/layouts/resource/resource.tpl.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file
- * Template for Resource Page.
+ * Template for Panopoly Burr Flipped.
  *
  * Variables:
  * - $css_id: An optional CSS id to use for the layout.
@@ -9,26 +9,40 @@
  * panel of the layout. This layout supports the following sections:
  */
 ?>
-
-<div class="panel-display resource clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="row">
-    <div class="col-md-8 content panel-panel">
-      <div class="panel-panel-inner">
-        <?php print $content['contentmain']; ?>
-      </div>
-    </div>
-    <div class="col-md-4  sidebar panel-panel">
-      <div class="row">
-        <div class="panel-panel-inner jumbotron">
-          <div>This resource is referenced in:</div>
-          <?php print $content['sidebartop']; ?>
+<section class="content-abstract">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 panel-panel highlighted">
+        <div class="panel-panel-inner">
+          <?php print $content['title']; ?>
         </div>
       </div>
+    </div>     
+  </div>
+</section>
+
+<section class="inner-page">
+  <div class="container page-content">
+  <a id="main-content"></a>        
+    <div class="row">
+      <div class="col-md-8 content panel-panel">
+        <div class="panel-panel-inner">
+          <?php print $content['contentmain']; ?>
+        </div>
+      </div>
+      <div class="col-md-4 sidebar panel-panel">
+        <div class="panel-panel-inner">
+         <div class="row">
+          <div class="panel-panel-inner no-jumbotron">
+            <div>This resource is referenced in:</div>
+            <?php print $content['sidebartop']; ?>
+          </div>
+        </div>
       <div class="row">
         <div class="panel-panel-inner">
           <?php print $content['sidebarbottom']; ?>
         </div>
       </div>
-    </div>
+    </div>    
   </div>
-</div><!-- /.resource -->
+</section>
