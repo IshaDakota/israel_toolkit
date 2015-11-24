7c003896a56e8294e2fefd190e94feb52964b7e9
IshaDakota <adam@marchinglizard.net>
2015-11-24 11:18:23 -0500

    Kristy's edits

--- toolkit/themes/custom/bootstrap_toolkit/templates/field--field_age_range.tpl.php
+++ toolkit/themes/custom/bootstrap_toolkit/templates/field--field_age_range.tpl.php
@@ -1,6 +1,6 @@
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if (!$label_hidden): ?>
-    <div class="field-label"<?php print $title_attributes; ?>>Themes&nbsp;<span class="glyphicon glyphicon-question-sign"></span></div>
+    <div class="field-label"<?php print $title_attributes; ?>>Themes&nbsp;<span class="glyphicon glyphicon-info-sign"></span></div>
   <?php endif; ?>
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
